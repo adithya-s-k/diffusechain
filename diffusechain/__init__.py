795dd88b7a78758d5cc68aaa91f726ecc26e54e3
Adithya S K <adithyaskolavi@gmail.com>
2023-07-09 04:01:46 +0530

    added txt2img from automatic1111 - Adithya S K

---
+++ diffusechain/__init__.py
@@ -0,0 +1,16 @@
+from .diffusechain import (
+    Automatic1111,
+    Automatic1111Result,
+    b64_img,
+    ControlNetUnit,
+)
+
+__version__ = "0.1.3"
+
+__all__ = [
+    "__version__",
+    "Automatic1111",
+    "Automatic1111Result",
+    "b64_img",
+    "ControlNetUnit",
+]
--- diffusechain/diffusechain.py
+++ diffusechain/diffusechain.py
@@ -1,1 +1,465 @@
-print("Hello world")
+import json
+import requests
+import io
+import base64
+from PIL import Image, PngImagePlugin
+from dataclasses import dataclass
+from enum import Enum
+from typing import List, Dict, Any
+
+
+class Upscaler(str, Enum):
+    none = "None"
+    Lanczos = "Lanczos"
+    Nearest = "Nearest"
+    LDSR = "LDSR"
+    BSRGAN = "BSRGAN"
+    ESRGAN_4x = "ESRGAN_4x"
+    R_ESRGAN_General_4xV3 = "R-ESRGAN General 4xV3"
+    ScuNET_GAN = "ScuNET GAN"
+    ScuNET_PSNR = "ScuNET PSNR"
+    SwinIR_4x = "SwinIR 4x"
+
+
+class HiResUpscaler(str, Enum):
+    none = "None"
+    Latent = "Latent"
+    LatentAntialiased = "Latent (antialiased)"
+    LatentBicubic = "Latent (bicubic)"
+    LatentBicubicAntialiased = "Latent (bicubic antialiased)"
+    LatentNearest = "Latent (nearist)"
+    LatentNearestExact = "Latent (nearist-exact)"
+    Lanczos = "Lanczos"
+    Nearest = "Nearest"
+    ESRGAN_4x = "ESRGAN_4x"
+    LDSR = "LDSR"
+    ScuNET_GAN = "ScuNET GAN"
+    ScuNET_PSNR = "ScuNET PSNR"
+    SwinIR_4x = "SwinIR 4x"
+
+
+@dataclass
+class Automatic1111Result:
+    images: list
+    parameters: dict
+    info: dict
+
+    @property
+    def image(self):
+        return self.images[0]
+
+
+class ControlNetUnit:
+    def __init__(
+        self,
+        input_image: Image = None,
+        mask: Image = None,
+        module: str = "none",
+        model: str = "None",
+        weight: float = 1.0,
+        resize_mode: str = "Resize and Fill",
+        lowvram: bool = False,
+        processor_res: int = 512,
+        threshold_a: float = 64,
+        threshold_b: float = 64,
+        guidance: float = 1.0,
+        guidance_start: float = 0.0,
+        guidance_end: float = 1.0,
+        control_mode: int = 0,
+        pixel_perfect: bool = False,
+        guessmode: int = None,  # deprecated: use control_mode
+    ):
+        self.input_image = input_image
+        self.mask = mask
+        self.module = module
+        self.model = model
+        self.weight = weight
+        self.resize_mode = resize_mode
+        self.lowvram = lowvram
+        self.processor_res = processor_res
+        self.threshold_a = threshold_a
+        self.threshold_b = threshold_b
+        self.guidance = guidance
+        self.guidance_start = guidance_start
+        self.guidance_end = guidance_end
+        if guessmode:
+            print(
+                "ControlNetUnit guessmode is deprecated. Please use control_mode instead."
+            )
+            control_mode = guessmode
+        self.control_mode = control_mode
+        self.pixel_perfect = pixel_perfect
+
+    def to_dict(self):
+        return {
+            "input_image": raw_b64_img(self.input_image) if self.input_image else "",
+            "mask": raw_b64_img(self.mask) if self.mask is not None else None,
+            "module": self.module,
+            "model": self.model,
+            "weight": self.weight,
+            "resize_mode": self.resize_mode,
+            "lowvram": self.lowvram,
+            "processor_res": self.processor_res,
+            "threshold_a": self.threshold_a,
+            "threshold_b": self.threshold_b,
+            "guidance": self.guidance,
+            "guidance_start": self.guidance_start,
+            "guidance_end": self.guidance_end,
+            "control_mode": self.control_mode,
+            "pixel_perfect": self.pixel_perfect,
+        }
+
+
+def b64_img(image: Image) -> str:
+    return "data:image/png;base64," + raw_b64_img(image)
+
+
+def raw_b64_img(image: Image) -> str:
+    # XXX controlnet only accepts RAW base64 without headers
+    with io.BytesIO() as output_bytes:
+        metadata = None
+        for key, value in image.info.items():
+            if isinstance(key, str) and isinstance(value, str):
+                if metadata is None:
+                    metadata = PngImagePlugin.PngInfo()
+                metadata.add_text(key, value)
+        image.save(output_bytes, format="PNG", pnginfo=metadata)
+
+        bytes_data = output_bytes.getvalue()
+
+    return str(base64.b64encode(bytes_data), "utf-8")
+
+
+class Automatic1111:
+    has_controlnet = False
+
+    def __init__(
+        self,
+        host="127.0.0.1",
+        port=7860,
+        baseurl=None,
+        sampler="Euler a",
+        steps=20,
+        use_https=False,
+        username=None,
+        password=None,
+    ):
+        if baseurl is None:
+            if use_https:
+                baseurl = f"https://{host}:{port}/sdapi/v1"
+            else:
+                baseurl = f"http://{host}:{port}/sdapi/v1"
+
+        self.baseurl = baseurl
+        self.default_sampler = sampler
+        self.default_steps = steps
+
+        self.session = requests.Session()
+
+        if username and password:
+            self.set_auth(username, password)
+        else:
+            self.check_controlnet()
+
+    def check_controlnet(self):
+        try:
+            scripts = self.get_scripts()
+            self.has_controlnet = "controlnet m2m" in scripts["txt2img"]
+        except:
+            pass
+
+    def set_auth(self, username, password):
+        self.session.auth = (username, password)
+        self.check_controlnet()
+
+    def _to_api_result(self, response):
+        if response.status_code != 200:
+            raise RuntimeError(response.status_code, response.text)
+
+        r = response.json()
+        images = []
+        if "images" in r.keys():
+            images = [Image.open(io.BytesIO(base64.b64decode(i))) for i in r["images"]]
+        elif "image" in r.keys():
+            images = [Image.open(io.BytesIO(base64.b64decode(r["image"])))]
+
+        info = ""
+        if "info" in r.keys():
+            try:
+                info = json.loads(r["info"])
+            except:
+                info = r["info"]
+        elif "html_info" in r.keys():
+            info = r["html_info"]
+        elif "caption" in r.keys():
+            info = r["caption"]
+
+        parameters = ""
+        if "parameters" in r.keys():
+            parameters = r["parameters"]
+
+        return Automatic1111Result(images, parameters, info)
+
+    async def _to_api_result_async(self, response):
+        if response.status != 200:
+            raise RuntimeError(response.status, await response.text())
+
+        r = await response.json()
+        images = []
+        if "images" in r.keys():
+            images = [Image.open(io.BytesIO(base64.b64decode(i))) for i in r["images"]]
+        elif "image" in r.keys():
+            images = [Image.open(io.BytesIO(base64.b64decode(r["image"])))]
+
+        info = ""
+        if "info" in r.keys():
+            try:
+                info = json.loads(r["info"])
+            except:
+                info = r["info"]
+        elif "html_info" in r.keys():
+            info = r["html_info"]
+        elif "caption" in r.keys():
+            info = r["caption"]
+
+        parameters = ""
+        if "parameters" in r.keys():
+            parameters = r["parameters"]
+
+        return Automatic1111Result(images, parameters, info)
+
+    def txt2img(
+        self,
+        enable_hr=False,
+        denoising_strength=0.7,
+        firstphase_width=0,
+        firstphase_height=0,
+        hr_scale=2,
+        hr_upscaler=HiResUpscaler.Latent,
+        hr_second_pass_steps=0,
+        hr_resize_x=0,
+        hr_resize_y=0,
+        prompt="",
+        styles=[],
+        seed=-1,
+        subseed=-1,
+        subseed_strength=0.0,
+        seed_resize_from_h=0,
+        seed_resize_from_w=0,
+        sampler_name=None,  # use this instead of sampler_index
+        batch_size=1,
+        n_iter=1,
+        steps=None,
+        cfg_scale=7.0,
+        width=512,
+        height=512,
+        restore_faces=False,
+        tiling=False,
+        do_not_save_samples=False,
+        do_not_save_grid=False,
+        negative_prompt="",
+        eta=1.0,
+        s_churn=0,
+        s_tmax=0,
+        s_tmin=0,
+        s_noise=1,
+        override_settings={},
+        override_settings_restore_afterwards=True,
+        script_args=None,  # List of arguments for the script "script_name"
+        script_name=None,
+        send_images=True,
+        save_images=False,
+        alwayson_scripts={},
+        controlnet_units: List[ControlNetUnit] = [],
+        sampler_index=None,  # deprecated: use sampler_name
+        use_deprecated_controlnet=False,
+        use_async=False,
+    ):
+        if sampler_index is None:
+            sampler_index = self.default_sampler
+        if sampler_name is None:
+            sampler_name = self.default_sampler
+        if steps is None:
+            steps = self.default_steps
+        if script_args is None:
+            script_args = []
+        payload = {
+            "enable_hr": enable_hr,
+            "hr_scale": hr_scale,
+            "hr_upscaler": hr_upscaler,
+            "hr_second_pass_steps": hr_second_pass_steps,
+            "hr_resize_x": hr_resize_x,
+            "hr_resize_y": hr_resize_y,
+            "denoising_strength": denoising_strength,
+            "firstphase_width": firstphase_width,
+            "firstphase_height": firstphase_height,
+            "prompt": prompt,
+            "styles": styles,
+            "seed": seed,
+            "subseed": subseed,
+            "subseed_strength": subseed_strength,
+            "seed_resize_from_h": seed_resize_from_h,
+            "seed_resize_from_w": seed_resize_from_w,
+            "batch_size": batch_size,
+            "n_iter": n_iter,
+            "steps": steps,
+            "cfg_scale": cfg_scale,
+            "width": width,
+            "height": height,
+            "restore_faces": restore_faces,
+            "tiling": tiling,
+            "do_not_save_samples": do_not_save_samples,
+            "do_not_save_grid": do_not_save_grid,
+            "negative_prompt": negative_prompt,
+            "eta": eta,
+            "s_churn": s_churn,
+            "s_tmax": s_tmax,
+            "s_tmin": s_tmin,
+            "s_noise": s_noise,
+            "override_settings": override_settings,
+            "override_settings_restore_afterwards": override_settings_restore_afterwards,
+            "sampler_name": sampler_name,
+            "sampler_index": sampler_index,
+            "script_name": script_name,
+            "script_args": script_args,
+            "send_images": send_images,
+            "save_images": save_images,
+            "alwayson_scripts": alwayson_scripts,
+        }
+
+        if use_deprecated_controlnet and controlnet_units and len(controlnet_units) > 0:
+            payload["controlnet_units"] = [x.to_dict() for x in controlnet_units]
+            return self.custom_post(
+                "controlnet/txt2img", payload=payload, use_async=use_async
+            )
+
+        if controlnet_units and len(controlnet_units) > 0:
+            payload["alwayson_scripts"]["ControlNet"] = {
+                "args": [x.to_dict() for x in controlnet_units]
+            }
+        elif self.has_controlnet:
+            # workaround : if not passed, webui will use previous args!
+            payload["alwayson_scripts"]["ControlNet"] = {"args": []}
+
+        return self.post_and_get_api_result(
+            f"{self.baseurl}/txt2img", payload, use_async
+        )
+
+    def post_and_get_api_result(self, url, json, use_async):
+        if use_async:
+            import asyncio
+
+            return asyncio.ensure_future(self.async_post(url=url, json=json))
+        else:
+            response = self.session.post(url=url, json=json)
+            return self._to_api_result(response)
+
+    async def async_post(self, url, json):
+        import aiohttp
+
+        async with aiohttp.ClientSession() as session:
+            auth = aiohttp.BasicAuth(self.session.auth[0], self.session.auth[1]) if self.session.auth else None
+            async with session.post(url, json=json, auth=auth) as response:
+                return await self._to_api_result_async(response)
+
+    
+
+    def extra_single_image(
+        self,
+        image,  # PIL Image
+        resize_mode=0,
+        show_extras_results=True,
+        gfpgan_visibility=0,
+        codeformer_visibility=0,
+        codeformer_weight=0,
+        upscaling_resize=2,
+        upscaling_resize_w=512,
+        upscaling_resize_h=512,
+        upscaling_crop=True,
+        upscaler_1="None",
+        upscaler_2="None",
+        extras_upscaler_2_visibility=0,
+        upscale_first=False,
+        use_async=False,
+    ):
+        payload = {
+            "resize_mode": resize_mode,
+            "show_extras_results": show_extras_results,
+            "gfpgan_visibility": gfpgan_visibility,
+            "codeformer_visibility": codeformer_visibility,
+            "codeformer_weight": codeformer_weight,
+            "upscaling_resize": upscaling_resize,
+            "upscaling_resize_w": upscaling_resize_w,
+            "upscaling_resize_h": upscaling_resize_h,
+            "upscaling_crop": upscaling_crop,
+            "upscaler_1": upscaler_1,
+            "upscaler_2": upscaler_2,
+            "extras_upscaler_2_visibility": extras_upscaler_2_visibility,
+            "upscale_first": upscale_first,
+            "image": b64_img(image),
+        }
+
+        return self.post_and_get_api_result(
+            f"{self.baseurl}/extra-single-image", payload, use_async
+        )
+
+    def extra_batch_images(
+        self,
+        images,  # list of PIL images
+        name_list=None,  # list of image names
+        resize_mode=0,
+        show_extras_results=True,
+        gfpgan_visibility=0,
+        codeformer_visibility=0,
+        codeformer_weight=0,
+        upscaling_resize=2,
+        upscaling_resize_w=512,
+        upscaling_resize_h=512,
+        upscaling_crop=True,
+        upscaler_1="None",
+        upscaler_2="None",
+        extras_upscaler_2_visibility=0,
+        upscale_first=False,
+        use_async=False,
+    ):
+        if name_list is not None:
+            if len(name_list) != len(images):
+                raise RuntimeError("len(images) != len(name_list)")
+        else:
+            name_list = [f"image{i + 1:05}" for i in range(len(images))]
+        images = [b64_img(x) for x in images]
+
+        image_list = []
+        for name, image in zip(name_list, images):
+            image_list.append({"data": image, "name": name})
+
+        payload = {
+            "resize_mode": resize_mode,
+            "show_extras_results": show_extras_results,
+            "gfpgan_visibility": gfpgan_visibility,
+            "codeformer_visibility": codeformer_visibility,
+            "codeformer_weight": codeformer_weight,
+            "upscaling_resize": upscaling_resize,
+            "upscaling_resize_w": upscaling_resize_w,
+            "upscaling_resize_h": upscaling_resize_h,
+            "upscaling_crop": upscaling_crop,
+            "upscaler_1": upscaler_1,
+            "upscaler_2": upscaler_2,
+            "extras_upscaler_2_visibility": extras_upscaler_2_visibility,
+            "upscale_first": upscale_first,
+            "imageList": image_list,
+        }
+
+        return self.post_and_get_api_result(
+            f"{self.baseurl}/extra-batch-images", payload, use_async
+        )
+
+    # XXX 500 error (2022/12/26)
+    def png_info(self, image):
+        payload = {
+            "image": b64_img(image),
+        }
+
+        response = self.session.post(url=f"{self.baseurl}/png-info", json=payload)
+        return self._to_api_result(response)
+
